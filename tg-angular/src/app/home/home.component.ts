d4b4a0d50725438d3244022d60c8172ced2e7c59
militachindarkar <62513035+militachindarkar@users.noreply.github.com>
2020-09-23 17:43:03 +0530

    remove errors

--- tg-angular/src/app/home/home.component.ts
+++ tg-angular/src/app/home/home.component.ts
@@ -1,9 +1,7 @@
-import { Feature } from './../../../../WeatherApp/weather-app/src/app/shared/mapbox-service.service';
 import { HomeData } from './../shared/home-data';
 import { TableData } from './../shared/table-data';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
-import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-home',
